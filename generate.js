40ac4bfdc66237d5f63c60c79f6f648bdbd0a860
Restyled.io <commits@restyled.io>
2023-07-11 13:53:44 +0000

    Restyled by prettier

--- generate.js
+++ generate.js
@@ -1,42 +1,50 @@
 // Generate the static .json files
-import fs from 'fs'
+import fs from "fs";
 
-import {contacts, labels, org, referrals, reviews, socials, team} from './data'
+import {
+  contacts,
+  labels,
+  org,
+  referrals,
+  reviews,
+  socials,
+  team,
+} from "./data";
 
-export const publicOrgPathBase = '.'
+export const publicOrgPathBase = ".";
 
-export const publicOrgPathProfile = `${publicOrgPathBase}/public/profile`
-export const publicOrgPathPublic = `${publicOrgPathProfile}/public`
-export const publicOrgPathData = `${publicOrgPathPublic}/data`
+export const publicOrgPathProfile = `${publicOrgPathBase}/public/profile`;
+export const publicOrgPathPublic = `${publicOrgPathProfile}/public`;
+export const publicOrgPathData = `${publicOrgPathPublic}/data`;
 
-export function publicOrgGenerateDataJson(name = 'github', data = {}) {
-  const path = `${publicOrgPathData}/${name}.json`
-  fs.writeFileSync(path, JSON.stringify(data, null, 2))
-  console.log(`publicOrgGenerateDataJson: generating  ${path}`, data)
+export function publicOrgGenerateDataJson(name = "github", data = {}) {
+  const path = `${publicOrgPathData}/${name}.json`;
+  fs.writeFileSync(path, JSON.stringify(data, null, 2));
+  console.log(`publicOrgGenerateDataJson: generating  ${path}`, data);
 }
 
 export function publicOrgGenerateGenerate() {
-  publicOrgGenerateDataJson('github', {org, labels})
-  publicOrgGenerateDataJson('contacts', contacts)
-  publicOrgGenerateDataJson('referrals', referrals)
-  publicOrgGenerateDataJson('reviews', reviews)
-  publicOrgGenerateDataJson('socials', socials)
-  publicOrgGenerateDataJson('team', team)
+  publicOrgGenerateDataJson("github", { org, labels });
+  publicOrgGenerateDataJson("contacts", contacts);
+  publicOrgGenerateDataJson("referrals", referrals);
+  publicOrgGenerateDataJson("reviews", reviews);
+  publicOrgGenerateDataJson("socials", socials);
+  publicOrgGenerateDataJson("team", team);
 
   // Generate the static profile README.md file
-  hbs('README.md', publicOrgPathProfile)
-  hbs('static/humans.txt', publicOrgPathProfile)
+  hbs("README.md", publicOrgPathProfile);
+  hbs("static/humans.txt", publicOrgPathProfile);
 
-  console.log('running static/generate.js', {
-    src : {
-      basePath : publicOrgPathBase,
-      pathPublic : publicOrgPathPublic,
-      pathPublicData : publicOrgPathData,
+  console.log("running static/generate.js", {
+    src: {
+      basePath: publicOrgPathBase,
+      pathPublic: publicOrgPathPublic,
+      pathPublicData: publicOrgPathData,
     },
-  })
+  });
 
-  let data = new Socials().data
-  let langs = new Socials().langs
-  console.log('data', data)
-  console.log('langs', langs)
+  let data = new Socials().data;
+  let langs = new Socials().langs;
+  console.log("data", data);
+  console.log("langs", langs);
 }
